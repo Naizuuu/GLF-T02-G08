81009c3d785d2924893d46dcf6a7717a9996a05b
Sebastián <sebazmo@gmail.com>
2020-10-23 01:41:15 -0300

    Leves cambios al front

--- resources/views/automatas/home.blade.php
+++ resources/views/automatas/home.blade.php
@@ -6,14 +6,33 @@
 
     <h1 class="text-center display-1">¿Qué tipo de autómatas ingresará?</h1>
     <div class="container">
+
+      <form style="margin-top: 5%;" method="GET">
         <div class="row">
-          <div class="col-sm">
-            <h3>Autómata finito determinista (AFD)</h2>
+          <div class="col-sm"> 
+              <div class="input-group" style="margin-top: 3%;">
+                  <select class="custom-select" name="automataUno">
+                      <option value="AFD">AFD</option>
+                      <option value="AFND" disabled>AFND</option>  {{-- AFND está bloqueado ya que aún no pasamos esa materia --}}
+                  </select>
+              </div>
           </div>
           <div class="col-sm">
-            <h3>Autómata finito no determinista (AFND) </h2>
+              <div class="input-group" style="margin-top: 3%;">
+                <select class="custom-select" name="automataDos">
+                  <label for="automataDos">uwu</label>
+                  <option value="AFD" >AFD</option>
+                  <option value="AFND" disabled>AFND</option>  {{-- AFND está bloqueado ya que aún no pasamos esa materia --}}
+                </select>
+              </div>
           </div>
         </div>
-    </div>   
+        <button style="" type="submit" class="btn btn-success btn-lg btn-block custom-btn" onclick="">Confirmar</button> 
+
+
+
+      </form>
+
+  </div>
 
 @endsection
